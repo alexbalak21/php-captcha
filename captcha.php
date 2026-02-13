f5692a05c6bcf79e182dfacdf669c342a2e59b63
alexbalak21 <alex.balak@outlook.com>
2026-02-13 12:42:26 +0100

    updated to oop

--- captcha.php
+++ captcha.php
@@ -1,55 +1,60 @@
 <?php
 session_start();
 
-// Generate 3 random uppercase letters or numbers (excluding ambiguous ones)
-$captcha_chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789'; // Exclude O, I, 0, 1
-$captcha = substr(str_shuffle($captcha_chars), 0, 3);
-$_SESSION['captcha'] = $captcha;
+class Captcha {
+	private $chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+	private $length = 3;
+	private $code;
+	private $img;
+	private $different_levels = true;
 
-// Create image
-$img = imagecreatetruecolor(120, 40);
+	public function __construct($different_levels = true) {
+		$this->different_levels = $different_levels;
+		$this->generateCode();
+		$_SESSION['captcha'] = $this->code;
+		$this->createImage();
+	}
 
-// Colors
-$bg = imagecolorallocate($img, 240, 240, 240);
-$fg = imagecolorallocate($img, 20, 20, 20);
-$line_color = imagecolorallocate($img, 120, 120, 120);
+	private function generateCode() {
+		$this->code = substr(str_shuffle($this->chars), 0, $this->length);
+	}
 
-// Fill background
-imagefilledrectangle($img, 0, 0, 120, 40, $bg);
-
-// Add random lines for noise
-for ($i = 0; $i < 5; $i++) {
-	imageline(
-		$img,
-		rand(0, 120), rand(0, 40),
-		rand(0, 120), rand(0, 40),
-		$line_color
-	);
-}
-
-// Add each letter with random rotation and y-offset
-for ($i = 0; $i < 3; $i++) {
-	$letter = $captcha[$i];
-	$angle = rand(-30, 30); // random rotation
-	// Larger font size for TTF
-	$font_size = rand(34, 38); // make font size slightly larger for TTF
-	$x = 10 + $i * 37 + rand(-2, 2); // adjust x position for even larger letters
-	$y = rand(34, 39); // adjust y position for better vertical centering
-	if (function_exists('imagettftext')) {
-		$font_file = __DIR__ . '/arial.ttf'; // You need to provide a TTF font file
-		if (file_exists($font_file)) {
-			imagettftext($img, $font_size, $angle, $x, $y, $fg, $font_file, $letter);
-		} else {
-			// fallback to imagestring if no TTF font
-			imagestring($img, 5, $x, $y - 20, $letter, $fg);
+	private function createImage() {
+		$this->img = imagecreatetruecolor(120, 40);
+		$bg = imagecolorallocate($this->img, 240, 240, 240);
+		$fg = imagecolorallocate($this->img, 20, 20, 20);
+		$line_color = imagecolorallocate($this->img, 120, 120, 120);
+		imagefilledrectangle($this->img, 0, 0, 120, 40, $bg);
+		for ($i = 0; $i < 5; $i++) {
+			imageline(
+				$this->img,
+				rand(0, 120), rand(0, 40),
+				rand(0, 120), rand(0, 40),
+				$line_color
+			);
 		}
-	} else {
-		imagestring($img, 5, $x, $y - 20, $letter, $fg);
+		for ($i = 0; $i < $this->length; $i++) {
+			$letter = $this->code[$i];
+			$angle = rand(-30, 30);
+			$font_size = rand(34, 38);
+			$x = 10 + $i * 37 + rand(-2, 2);
+			$y = $this->different_levels ? rand(34, 39) : 39;
+			$use_ttf = function_exists('imagettftext') && file_exists(__DIR__ . '/arial.ttf');
+			if ($use_ttf) {
+				$font_file = __DIR__ . '/arial.ttf';
+				imagettftext($this->img, $font_size, $angle, $x, $y, $fg, $font_file, $letter);
+			} else {
+				imagestring($this->img, 5, $x, $y - 20 + ($this->different_levels ? rand(-6, 6) : 0), $letter, $fg);
+			}
+		}
+	}
+
+	public function output() {
+		header('Content-Type: image/png');
+		imagepng($this->img);
+		// imagedestroy($this->img); // Deprecated, can be omitted
 	}
 }
 
-// Output as PNG image
-header('Content-Type: image/png');
-imagepng($img);
-imagedestroy($img);
-?>
+$captcha = new Captcha(true); // Set to false for aligned letters
+$captcha->output();
